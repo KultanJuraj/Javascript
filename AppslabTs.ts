89083ab6e99d42c7b493ac32bbb7a0f66c479426
Juraj Kultán <filipenkodavid@gmail.com>
2020-10-08 09:29:27 +0200

    string

--- AppslabTs.ts
+++ AppslabTs.ts
@@ -1,1 +1,3 @@
 console.log("Hello AppsLab 2020!");
+var string: string="This is string";
+console.log(string);
---
+++ out/AppslabTs.js
@@ -0,0 +1,4 @@
+console.log("Hello AppsLab 2020!");
+var string = "This is string";
+console.log(string);
+//# sourceMappingURL=AppslabTs.js.map
