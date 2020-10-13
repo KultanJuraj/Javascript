fed6286ce42cbe2d9aaa506cfbfd27b5e9c0a460
Kultán Juraj <kultanj1@gmail.com>
2020-10-13 16:46:05 +0200

    Snail go up the stairs.

---
+++ Snail_go _up the_stairs.ts
@@ -0,0 +1,6 @@
+var StairH = 0.5;
+var StairW = 0.4;
+var TowerH = 150;
+var NumberOfStairs =TowerH/StairH;
+var TotalDistance = NumberOfStairs * (StairH+StairW);
+console.log("Total distance snail must gov is:" + TotalDistance);
