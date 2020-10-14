1534fc2a3270c2a85e0843db74e591039f2d1b5b
Kultán Juraj <kultanj1@gmail.com>
2020-10-14 20:09:32 +0200

    Triangle

---
+++ Triangle.ts
@@ -0,0 +1,11 @@
+var SiteA = 2;
+var SiteB = 30;
+var SiteC = 4;
+if (SiteA<=(SiteB+SiteC)&&(SiteA+SiteB)>=SiteC&&SiteB<=(SiteA+SiteC)){
+    var triangle = true;
+    console.log(triangle)
+}
+else {
+   var triangle = false;
+    console.log(triangle)
+}
